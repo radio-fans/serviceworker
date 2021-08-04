28a5b02891f2136f6cd1d047bcb679907ff025d7
Radio Fans <69336314+radio-fans@users.noreply.github.com>
2021-08-04 02:33:03 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,14 @@
+addEventListener('install', event => {
+	const preCache = async () => {
+		const cache = await caches.open('radiofansreceptorcache');
+		return cache.addAll([
+			'',
+			'logo.js'
+		]);
+	};
+	event.waitUntil(preCache());
+});
+
 self.addEventListener('fetch', function(e) {
 	e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
 });
