e5ea2d7a618ff18a9d5dc6e5d8c451d2f5861bd4
Radio Fans <69336314+radio-fans@users.noreply.github.com>
2021-08-03 23:07:09 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
 self.addEventListener('fetch', async function(e) {
-	caches.match(e.request).then(r => e.respondWith(r || fetch(e.request)));
+ const r = await caches.match(e.request);
+ e.respondWith(r || fetch(e.request));
 });
